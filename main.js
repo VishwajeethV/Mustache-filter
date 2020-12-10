5af8af3127067745e684e675f63fcaa391e7f4ed
VishwajeethV <74006085+VishwajeethV@users.noreply.github.com>
2020-12-10 18:47:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+var nose_x =0;
+var nose_y =0;
+
+function preload() {
+    mustache_image = loadImage("https://i.postimg.cc/wTcJxW0p/m.png");
+};
+
+function setup() {
+canvas=createCanvas(350,300);
+canvas.center();
+video=createCapture(VIDEO);
+video.hide();
+posenet=ml5.poseNet(video,modelLoaded);
+posenet.on("pose",getpose);
+}
+
+function modelLoaded()  {
+console.log('modelLoaded');
+}
+
+
+
+function getpose(result) {
+if(result.length>0) {
+    console.log(result);
+    nose_x = result[0].pose.nose.x;
+    nose_y = result[0].pose.nose.y;
+  }
+}
+
+
+function draw() {
+    image(video,0,0,350,300);
+    image(mutache_image,nose_x-30,nose_y-30,30,30);
+}
+
+function take_snapshot() {
+
+    save("mustache.jpg");
+   }
